9a7451ac995c7f787e1413b9423f8c4044b1e6b0
OmegamonX <jingbo.wang.0440@gmail.com>
2023-09-11 09:45:09 -0400

    Changed Multiples.java to not include 1000

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,7 +1,7 @@
 public class Multiples {
     public static void main(String[] args) {
         int result = 0;
-        for (int i = 1; i < 1001; i++){
+        for (int i = 1; i < 1000; i++){
             if (i % 3 == 0 || i % 5 == 0){
                 result ++;
             }
